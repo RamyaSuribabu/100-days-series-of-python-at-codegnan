fa19cdb8a82f577d21f7d335ea7d56571cc99c4d
RamyaSuribabu <ramyakaturu8@gmail.com>
2026-01-08 16:53:55 +0530

    Add files via upload
    day -1 Basics of python
    day-2 variables,io functions,datatypes
    day 3 assignment operators

---
+++ day2.py
@@ -0,0 +1,23 @@
+number = 12 #integer type 
+print(number)
+deci = 1.20 #float  type
+print(deci)
+name = "ramya" #string type
+print(name)
+age = 20 #integer
+print(age)
+lst=[1,2,3,4,{2,3,4},"hi",20.23,(1,2,3)] #list of items
+print(lst)
+tuple_1=( 1,2,3,4,5,"asf",6) #tuple of elements
+print(tuple_1)
+set_1 = { "A",1,2,3,4,5,6,7,8,9,10,"J","K","Q"} # set type
+print(set_1)
+student_details={ "name":"ramya","age":20,"address":"guntur"} # dict type
+print(student_details)
+print("my name is :",name)
+print("my age is :",age)
+print("my details are",student_details)
+print("list of items ",lst)
+print("tuple of elementx",tuple_1)
+print("set of cards",set_1)
+
---
+++ day3.py
@@ -0,0 +1,11 @@
+#simple calculator using arithmetic operations 
+a=20
+b=10
+print("sum of 2 numbers is :",a+b)
+print("difference of 2 numbers:",a-b)
+print("multiplication of 2 numbers:",a*b)
+print("division of 2 numbers is :",a/b)
+print("floor division of 2 numbers is:",a//b)
+print("modulo division of 2 numbers is:",a%b)
+print("power of 2 numbers:",a**b)
+print("10<20 is:",10<20)
