eba5a02a3afd40bea351271e43872853dfe3f2d5
RamyaSuribabu <ramyakaturu8@gmail.com>
2026-01-09 09:15:05 +0530

    Add files via upload
    operators

---
+++ day3(operators).py
@@ -0,0 +1,63 @@
+# relational operators
+a=10
+b=20
+print(a==b)
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+print(a!=b)
+#logical operators
+print(a<b and a>b)
+print(a<b and a>b)
+print(not True)
+print(not "")
+#assignent operators
+j=10
+j=j+10
+print(j)
+r=10
+p=2
+r+=p
+print(r)
+r-=p
+print(r)
+r*=p
+print(r)
+r/=p
+print(r)
+r//+p
+print(r)
+r%=p
+print(r)
+r**=p
+print(p)
+
+#bitwise operators
+print(3&5)
+print(3|5)
+print(3^5)
+print(~3)
+print(3<<5)
+print(3>>5)
+#identity operators
+k=10
+l=5
+print(k is l)
+print(k is not l)
+#membership operators
+a="asdfgh"
+print("asd" in a)
+print("fgh" not in a)
+
+
+
+
+
+
+
+
+
+
+
+
